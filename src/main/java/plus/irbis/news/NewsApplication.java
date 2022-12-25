f1ae96baf88f0d2b79852b4412eeff64e058b4a7
TumanovAnton <djdkdancik@gmail.com>
2022-12-25 10:39:18 +0400

    Initial commit

---
+++ src/main/java/plus/irbis/news/NewsApplication.java
@@ -0,0 +1,13 @@
+package plus.irbis.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApplication.class, args);
+	}
+
+}
